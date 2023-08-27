3ed86a0b3bca4b3a5605b24726cc78074ba435ed
Isaac <cookeisaac2@gmail.com>
2023-08-27 16:47:34 +0100

    Added password field to user

--- server/src/models/users.rs
+++ server/src/models/users.rs
@@ -6,11 +6,11 @@ pub struct User {
     pub id: i32,
     pub username: String,
     pub email: String,
+    pub password: String,
     pub created_at: chrono::NaiveDateTime,
 }
 
 #[derive!(Insertable, AsChangeset)]
-#[diesel(table_name = "users")]
 pub struct NewUser {
     pub username: String,
     pub email: String,
